19a6f6b60d9bee46131e25a5e4a62f24ae4614df
rajpal229 <rajpalrawat229@gmail.com>
2022-11-24 17:14:58 +0530

    Refacator: multiples times of flipping a coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,17 @@
+head=0;
+tail=0;
+
+for (( i=1; i<=10; i++ ))
+do
 flip=$((RANDOM%2))
 
 if [ $flip == 0 ]
 then
 	echo "Heads"
+	((head++))
 else
 	echo "Tails"
+	((tail++))
 fi
+done
+echo "$head times head won and $tail times tail won"
